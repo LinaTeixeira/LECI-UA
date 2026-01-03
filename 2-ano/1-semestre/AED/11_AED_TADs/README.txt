5067b89c954ef9e869d88aaeda1379921c210e8c
Carolina <carolinasteixeira@ua.pt>
2026-01-03 18:37:29 +0000

    cleanup

--- 2-ano/1-semestre/AED/11_AED_TADs/README.txt
+++
@@ -1,7 +0,0 @@
-
-Simple representations and functions for the Triangle abstract type.
-
-Compile with
-
-gcc -Wall -Wextra testing_Triangle.c Triangle.c -o testing -lm
-
--- 2-ano/1-semestre/AED/13_AED_Listas_Ligadas_I/03_LINKED_LIST/example_PointersList.c
+++
@@ -1,9 +0,0 @@
-//
-// Algoritmos e Estruturas de Dados --- 2024/2025
-//
-// Pointers LIST application example --- TO BE COMPLETED
-//
-
-#include "PointersList.h"
-
-int main(void) { return 0; }
